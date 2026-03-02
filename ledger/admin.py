aec7d77b67d7c779e1cf1a904400590f53e73624
Goldenslayer0422 <97289552+Goldenslayer0422@users.noreply.github.com>
2026-03-02 23:25:32 +0800

    Update admin.py

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Profile, Ingredient, Recipe, RecipeIngredient
 
+admin.site.register(Profile)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
 
 # Register your models here.
+
