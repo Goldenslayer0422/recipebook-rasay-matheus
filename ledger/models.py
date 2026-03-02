4c94fd7cde7483a78a172f01ffc51da8325ae7a4
Goldenslayer0422 <97289552+Goldenslayer0422@users.noreply.github.com>
2026-03-02 23:26:16 +0800

    Update models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,18 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Profile(models.Model):
+    userkey = models.OneToOneField(User, default=None, on_delete=models.CASCADE)
+    p_name = models.CharField(max_length = 50)
+    p_shortbio = models.TextField()
 
 class Ingredient(models.Model):
     ing_name = models.CharField(max_length=30)
 
 class Recipe(models.Model):
     reci_name = models.CharField(max_length=30)
+    author = models.ForeignKey(Profile, null=True, on_delete=models.CASCADE)
+    CreatedOn = models.DateTimeField(auto_now_add=True,null=True)
+    UpdatedOn = models.DateTimeField(auto_now=True,null=True)
 
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
     reci_ing = models.ForeignKey(Ingredient, default=None, on_delete=models.CASCADE)
     recipefield = models.ForeignKey(Recipe, default=None, on_delete=models.CASCADE)
-
-def __str__(self):
-    return self.ing_name
+    def __str__(self):
+        return self.ing_name
 
 
 # Create your models here.
+
