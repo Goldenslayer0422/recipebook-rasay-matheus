11fa9e6539e472580ff3b65f5e7066399caaf552
Goldenslayer0422 <97289552+Goldenslayer0422@users.noreply.github.com>
2026-03-12 23:38:48 +0800

    Update
    Added static and media file configurations.

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ledger',
-    'useraccounts'
+    # 'useraccounts'
 ]
 
-AUTH_USER_MODEL = 'ledger.User'
+# AUTH_USER_MODEL = 'ledger.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,6 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
