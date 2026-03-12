6f9d857ef34c78e90f803e0dddd9f77c2c29195b
Goldenslayer0422 <97289552+Goldenslayer0422@users.noreply.github.com>
2026-03-12 23:35:14 +0800

    Add RecipeImage model to admin registration

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Profile, Ingredient, Recipe, RecipeIngredient
+from .models import Profile, Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 admin.site.register(Profile)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
+admin.site.register(RecipeImage)
 
 # Register your models here.
+
 
