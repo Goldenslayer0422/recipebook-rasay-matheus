dda1ffe675aaa1ab50fc991d4711a42f02097cc2
Goldenslayer0422 <97289552+Goldenslayer0422@users.noreply.github.com>
2026-03-12 23:37:20 +0800

    Added urls
    Added routes for creating and viewing recipe details.

--- ledger/urls.py
+++ ledger/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("recipeslist", views.recipesPage, name="recipesPage"),
     path("recipe1", views.recipes1, name="recipe1"),
     path("recipe2", views.recipes2, name="recipe2"),
-    path("login", views.loginpage, name="login")
+    path("login", views.loginpage, name="login"),
+    path("recipe/add", views.recipe_create, name="recipemaking"),
+    path("recipe/<int:pk>", views.recipe_detail, name="recipe_detail")
+
 ]
 
-
-# admin: monch, rasayispogi
